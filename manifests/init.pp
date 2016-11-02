8fddf4f010312f542c2574752f1b615d57b8e693
Tommy Speigner <tspeigner@gmail.com>
2016-11-02 09:44:27 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,18 @@
 class vrosudo {
-  notify {"Hello, world!":}
+  user { 'vro':
+    ensure   => present,
+    gid      => '600',
+    uid      => '600',
+    comment  => 'VRO Plugin User',
+    shell    => '/bin/bash',
+    password => pw_hash('buckle fiat flawed', 'SHA-512', 'mysalt'),
+  }
 
+  file { '/etc/sudoers.d/vro':
+    ensure => file,
+    mode   => '0440',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/vrosudo/vro'
+  }
 }
