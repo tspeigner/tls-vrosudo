1269fa8dd9999b105ec7da72d0cdcfa6ac69b37b
Tommy Speigner <tspeigner@gmail.com>
2016-11-02 15:08:46 -0700

    puppet-vro-plugin user updated

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 class vrosudo {
-  user { 'vro':
+  user { 'puppet-vro-plugin':
     ensure   => present,
     comment  => 'VRO Plugin User',
     shell    => '/bin/bash',
     password => pw_hash('buckle fiat flawed', 'SHA-512', 'mysalt'),
   }
 
-  file { '/etc/sudoers.d/vro':
+  file { '/etc/sudoers.d/puppet-vro-plugin':
     ensure  => file,
     mode    => '0440',
     owner   => 'root',
