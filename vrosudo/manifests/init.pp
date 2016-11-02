7a27be81e8fb7e88330aef40720a5da4a918a605
Tommy S <tspeigner@gmail.com>
2016-11-02 10:21:07 -0700

    New files being uploaded.

---
+++ vrosudo/manifests/init.pp
@@ -0,0 +1,20 @@
+# vro module to create and manage a sudoers for the vro user.
+
+class vrosudo {
+  user { 'vro':
+    ensure   => present,
+    gid      => '600',
+    uid      => '600',
+    comment  => 'VRO Plugin User',
+    shell    => '/bin/bash',
+    password => pw_hash('buckle fiat flawed', 'SHA-512', 'mysalt'),
+  }
+
+  file { '/etc/sudoers.d/vro':
+    ensure => file,
+    mode   => '0440',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/vrosudo/vro'
+  }
+}
