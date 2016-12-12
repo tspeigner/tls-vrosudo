26a1c63d4cd35223344010507b63a2481a84a619
demo <demo@master.inf.puppet.vm>
2016-12-12 15:50:01 -0800

    modified

--- makevm/manifests/init.pp
+++ makevm/manifests/init.pp
@@ -2,6 +2,6 @@ class makevm {
   vsphere_vm { '/west1/vm/TSEs/tommy/vspheretest':
   ensure        => running,
   source        => '/west1/vm/Templates/windows-2012-updatedtools',
-  resource_pool => 'tse1',
+  resource_pool => '/tse1',
   }
 }
