32f9ac35728d8e0a10786338b4b003eef111a8be
Tommy Speigner <tspeigner@gmail.com>
2016-12-12 16:12:51 -0800

    Delete init.pp

--- makevm/manifests/init.pp
+++
@@ -1,7 +0,0 @@
-class makevm {
-  vsphere_vm { '/west1/vm/TSEs/tommy/vspheretest':
-  ensure        => running,
-  source        => '/west1/vm/Templates/windows-2012-updatedtools',
-  resource_pool => '/west1/tse1',
-  }
-}
