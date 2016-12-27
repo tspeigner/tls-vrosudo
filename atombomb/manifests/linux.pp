78d4292cc7508b258bc74d8b6bb06b663b333acb
tspeigner <tspeigner@gmail.com>
2016-12-27 14:06:36 -0800

    Updated linux.pp.

--- atombomb/manifests/linux.pp
+++ atombomb/manifests/linux.pp
@@ -1,18 +1,18 @@
 #atombomb
-#Install Atom on Windows.
+#Install Atom on Linux.
 
-class atombomb::windows {
-  
+class atombomb::linux{
+
   package { 'atom':
     ensure   => latest,
     provider => 'chocolatey',
   }
-  
+
   Package{
     provider => apm,
     ensure   => latest,
   }
 
    package {['linter','atom-lint','puppet-lint','language-puppet','sync-on-save','language-docker','advanced-open-file','file-icons','terminal-plus', 'aligner','aligner-puppet','atom-beautify','auto-detect-indentationb','file-type-icons','git-plus','highlight-selected','linter-puppet-lint','minimap','minimap-cursorline','minimap-highlight-selected','minimap-linter','minimap-pigments','minimap-selection','open-recent','project-manager' ]:}
- 
+
 }
