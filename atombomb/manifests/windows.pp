ced6267e1b091b9996163675e4ee7097e93248e2
tspeigner <tspeigner@gmail.com>
2016-12-27 14:15:31 -0800

    Updated windows.pp.

--- atombomb/manifests/windows.pp
+++ atombomb/manifests/windows.pp
@@ -1,18 +1,18 @@
 #atombomb
 #Install Atom on Windows.
-
+#'linter-puppet-lint',
 class atombomb::windows {
-  
+
   package { 'atom':
     ensure   => latest,
     provider => 'chocolatey',
   }
-  
+
   Package{
     provider => apm,
     ensure   => latest,
   }
 
-   package {['linter','atom-lint','puppet-lint','language-puppet','sync-on-save','language-docker','advanced-open-file','file-icons','terminal-plus', 'aligner','aligner-puppet','atom-beautify','auto-detect-indentationb','file-type-icons','git-plus','highlight-selected','linter-puppet-lint','minimap','minimap-cursorline','minimap-highlight-selected','minimap-linter','minimap-pigments','minimap-selection','open-recent','project-manager' ]:}
- 
+   package {['linter','atom-lint','puppet-lint','language-puppet','sync-on-save','language-docker','advanced-open-file','file-icons','terminal-plus', 'aligner','aligner-puppet','atom-beautify','auto-detect-indentationb','file-type-icons','git-plus','highlight-selected','minimap','minimap-cursorline','minimap-highlight-selected','minimap-linter','minimap-pigments','minimap-selection','open-recent','project-manager' ]:}
+
 }
