b31d845bb189f8a20c494e12c1799532532c500d
tspeigner <tspeigner@gmail.com>
2016-12-28 11:06:16 -0800

    moving things around

--- atombomb/manifests/windows.pp
+++ atombomb/manifests/windows.pp
@@ -13,6 +13,6 @@ class atombomb::windows {
     ensure   => latest,
   }
 
-   package {['linter','atom-lint','puppet-lint','language-puppet','sync-on-save','language-docker','advanced-open-file','file-icons','terminal-plus', 'aligner','aligner-puppet','atom-beautify','auto-detect-indentationb','file-type-icons','git-plus','highlight-selected','minimap','minimap-cursorline','minimap-highlight-selected','minimap-linter','minimap-pigments','minimap-selection','open-recent','project-manager' ]:}
+   package {['linter','atom-lint','linter-puppet','language-puppet','sync-on-save','language-docker','advanced-open-file','file-icons','terminal-plus', 'aligner','aligner-puppet','atom-beautify','auto-detect-indentationb','file-type-icons','git-plus','highlight-selected','minimap','minimap-cursorline','minimap-highlight-selected','minimap-linter','minimap-pigments','minimap-selection','open-recent','project-manager' ]:}
 
 }
